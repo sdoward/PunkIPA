ceaa4d2904dbef4567584285399531a1a73e2497
sdoward <sam.doward@gmail.com>
2016-10-02 20:23:13 +0200

    Unsubscribe from observable on destroy

--- app/src/main/kotlin/com/samdoward/beer/android/ui/BeerPresenter.kt
+++ app/src/main/kotlin/com/samdoward/beer/android/ui/BeerPresenter.kt
@@ -4,19 +4,21 @@ import com.samdoward.beer.android.data.Beer
 import com.samdoward.beer.android.data.BeerService
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import rx.subscriptions.Subscriptions
 
 class BeerPresenter(private val beerService: BeerService, private val beerView: BeerView) {
 
+    private var subscription = Subscriptions.empty()
 
     fun start() {
-        beerService.getBeers()
+        subscription = beerService.getBeers()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ beerView.displayBeers(it) }, { it.printStackTrace() })
     }
 
     fun stop() {
-
+        subscription.unsubscribe()
     }
 
 }
